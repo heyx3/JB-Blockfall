b0f58e824ef1453dd3ac9c09462cc09fac4dd170
heyx3 <manning.w@husky.neu.edu>
2016-06-16 17:14:52 -0400

    Partial work on ray-casting support

--- Blockfall/Assets/Scripts/Game Board/Board.cs
+++ Blockfall/Assets/Scripts/Game Board/Board.cs
@@ -107,6 +107,132 @@ namespace GameBoard
 
 			Reset(tempArray);
 		}
+
+		/// <summary>
+		/// Returns {-1, -1} if no position was hit.
+		/// </summary>
+		/// <param name="isBlockPassable">
+		/// Should return "false" if the given block can stop a ray, and "true" if it can't.
+		/// </param>
+		/// <param name="outHitDir">
+		/// If there was a hit, gets set to 0 if the hit was on the min/max X face,
+		/// or 1 if on the min/max Y face.
+		/// </param>
+		/// <param name="outHitT">
+		/// If there was a hit, gets set to the distance from the ray to the hit position.
+		/// </param>
+		/// <param name="outHitPos">
+		/// If there was a hit, gets set to the position of the ray's hit.
+		/// </param>
+		public Vector2i CastRay(Ray2D ray, Func<Vector2i, BlockTypes, bool> isBlockPassable,
+								ref Vector2 outHitPos, ref float outHitT, ref int outHitDir)
+		{
+			float epsilon = 0.001f;
+			Vector2 rayDirEpsilon = ray.direction * epsilon;
+			Vector2 rayInvDir = new Vector2(1.0f / ray.direction.x, 1.0f / ray.direction.y);
+
+			Vector2i startPosI = ToTilePos(ray.origin);
+
+			//Edge-case: the ray started in a solid block.
+			if (startPosI.x >= 0 && startPosI.x < Width &&
+				startPosI.y >= 0 && startPosI.y < Height &&
+				!isBlockPassable(startPosI, this[startPosI]))
+			{
+				//If the ray is facing towards the center of the block,
+				//    cast backwards to find where it intersects the wall.
+				//Otherwise, cast forward.
+				float _dir = 1.0f;
+				if (Vector2.Dot(ray.direction, ToWorldPos(startPosI) - ray.origin) > 0.0f)
+					_dir = -1.0f;
+
+				float dummyT;
+				Vector2 dummyP;
+				int dummyD;
+				uint _nHits = MyMath.RaycastBox(new Ray2D(ray.origin, ray.direction * _dir),
+												rayInvDir * _dir,
+												ToWorldRect(startPosI),
+											    ref outHitT, ref dummyT,
+												ref outHitPos, ref dummyP,
+											    ref outHitDir, ref dummyD);
+				UnityEngine.Assertions.Assert.IsTrue(_nHits == 1, "Expected 1 hit!");
+				return startPosI;
+			}
+
+
+			//Get the ray's start and end point in the world box.
+			Rect startWB = ToWorldRect(new Vector2i(0, 0)),
+				 endWB = ToWorldRect(new Vector2i(Width - 1, Height - 1)),
+				 worldBounds = Rect.MinMaxRect(startWB.xMin, startWB.yMin,
+											   endWB.xMax, endWB.yMax);
+			float startT = 0.0f,
+				  endT = 0.0f;
+			Vector2 startPos = Vector2.zero,
+					endPos = Vector2.zero;
+			int startD = -1,
+				endD = -1;
+			uint nHits = MyMath.RaycastBox(ray, rayInvDir, worldBounds,
+										   ref startT, ref endT,
+										   ref startPos, ref endPos,
+										   ref startD, ref endD);
+			//If there were no hits, the ray never touches the tile grid.
+			if (nHits == 0)
+			{
+				return new Vector2i(-1, -1);
+			}
+			//If there was only one hit, the ray started inside the tile grid.
+			else if (nHits == 1)
+			{
+				//Because there was only one hit, the "startX" variables right now actually store
+				//    the *end* of the ray.
+
+				endT = startT;
+				endPos = startPos;
+				endD = startD;
+
+				startT = 0.0f;
+				startPos = ray.origin;
+				startD = 0;
+			}
+
+
+			//Now cast through each block until a solid one is hit or we pass through the grid.
+
+			float t1 = float.NaN, t2 = float.NaN;
+			Vector2 p1 = Vector2.zero, p2 = Vector2.zero;
+			int d1 = -1, d2 = -1;
+			nHits = MyMath.RaycastBox(ray, rayInvDir, ToWorldRect(startPosI),
+								      ref t1, ref t2, ref p1, ref p2, ref d1, ref d2);
+			UnityEngine.Assertions.Assert.IsTrue(nHits > 0, "nHits is 0??");
+
+			//TODO: fix this loop.
+			while (startPosI.x >= 0 && startPosI.x < Width &&
+				   startPosI.y >= 0 && startPosI.y < Height)
+			{
+				if (isBlockPassable(currentPosI, this[currentPosI]))
+				{
+					//Move to the end of the block.
+					currentT = endT + epsilon;
+					currentPos = endPos + rayDirEpsilon;
+
+					currentPosI = ToTilePos(currentPos);
+
+					startT = endT - epsilon;
+					startPos = endPos - rayDirEpsilon;
+					startD = endD;
+				}
+				else
+				{
+					outHitT = t1;
+					outHitPos = p1;
+					outHitDir = d1;
+
+					return startPosI;
+				}
+			}
+
+			//Nothing was hit.
+			return new Vector2i(-1, -1);
+		}
 		
 
 		public BlockTypes this[Vector2i tileIndex]
---
+++ Blockfall/Assets/Scripts/Utility/MyMath.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using UnityEngine;
+
+
+public static class MyMath
+{
+	public static bool IsNearZero(this float f, float error) { return Math.Abs(f) <= error; }
+	public static bool IsNearEqual(this float f, float f2, float error) { return Math.Abs(f - f2) <= error; }
+
+
+	public static Vector2 Mult(this Vector2 a, Vector2 b) { return new Vector2(a.x * b.x, a.y * b.y); }
+
+
+	/// <summary>
+	/// Returns 0 if there were no intersections,
+	///     1 if there was one intersection (i.e. the ray started inside the box),
+	///     and 2 if there were two intersections.
+	/// The "outHitD" output parameters are either 0 or 1 --
+	///     0 means the intersection was along the X; 1 along the Y.
+	/// </summary>
+	/// <param name="rayInvDir">The precomputed reciprocal of the ray's direction.</param>
+	public static uint RaycastBox(Ray2D ray, Vector2 rayInvDir, Rect box,
+								  ref float outHitT1, ref float outHitT2,
+								  ref Vector2 outHitP1, ref Vector2 outHitP2,
+								  ref int outHitD1, ref int outHitD2)
+	{
+		Vector2 min = box.min,
+				max = box.max;
+		
+
+		//Exit early if the ray has no chance of hitting.
+		if ((ray.direction.x < 0.0f && ray.origin.x < min.x) ||
+			(ray.direction.x > 0.0f && ray.origin.x > max.x) ||
+			(ray.direction.y < 0.0f && ray.origin.y < min.y) ||
+			(ray.direction.y > 0.0f && ray.origin.y > max.y))
+		{
+			return 0;
+		}
+
+
+		//Find the closest two intersections.
+		RBStruct p1 = new RBStruct(),
+				 p2 = new RBStruct();
+		p1.T = float.PositiveInfinity;
+		p2.T = float.PositiveInfinity;
+		RBStruct tempP = new RBStruct();
+
+		const float epsilon = 0.0001f;
+
+		//X faces.
+		if (!ray.direction.x.IsNearZero(epsilon))
+		{
+			tempP.D = 0;
+
+			//Min X face.
+			tempP.T = (min.x - ray.origin.x) * rayInvDir.x;
+			float y = (ray.direction.y * tempP.T) + ray.origin.y;
+			if (tempP.T >= 0.0f && (y >= min.y && y <= max.y))
+			{
+				tempP.Pos = new Vector2(min.x, y);
+				RaycastBoxHelper(ref p1, ref p2, tempP);
+			}
+
+			//Max X face.
+			tempP.T = (max.x - ray.origin.x) * rayInvDir.x;
+			y = (ray.direction.y * tempP.T) + ray.origin.y;
+			if (tempP.T >= 0.0 && (y >= min.y && y <= max.y))
+			{
+				tempP.Pos = new Vector2(max.x, y);
+				RaycastBoxHelper(ref p1, ref p2, tempP);
+			}
+		}
+		//Y faces.
+		if (!ray.direction.y.IsNearZero(epsilon))
+		{
+			tempP.D = 1;
+
+			//Min Y face.
+			tempP.T = (min.y - ray.origin.y) * rayInvDir.y;
+			float x = (ray.direction.x * tempP.T) + ray.origin.x;
+			if (tempP.T >= 0.0f && (x >= min.x && x <= max.x))
+			{
+				tempP.Pos = new Vector2(x, min.y);
+				RaycastBoxHelper(ref p1, ref p2, tempP);
+			}
+
+			//Max Y face.
+			tempP.T = (max.y - ray.origin.y) * rayInvDir.y;
+			x = (ray.direction.x * tempP.T) + ray.origin.x;
+			if (tempP.T >= 0.0f && (x >= min.x && x <= max.x))
+			{
+				tempP.Pos = new Vector2(x, min.y);
+				RaycastBoxHelper(ref p1, ref p2, tempP);
+			}
+		}
+
+		if (p1.T == float.PositiveInfinity)
+		{
+			return 0;
+		}
+		else
+		{
+			outHitT1 = p1.T;
+			outHitP1 = p1.Pos;
+			outHitD1 = p1.D;
+
+			if (p2.T == float.PositiveInfinity)
+			{
+				return 1;
+			}
+			else
+			{
+				outHitT2 = p2.T;
+				outHitP2 = p2.Pos;
+				outHitD2 = p2.D;
+
+				return 2;
+			}
+		}
+	}
+	private struct RBStruct { public float T; public int D;  public Vector2 Pos; }
+	private static void RaycastBoxHelper(ref RBStruct p1, ref RBStruct p2, RBStruct newP)
+	{
+		if (p1.T > newP.T)
+		{
+			p2 = p1;
+			p1 = newP;
+		}
+		else if (p2.T > newP.T)
+		{
+			p2 = newP;
+		}
+	}
+}
